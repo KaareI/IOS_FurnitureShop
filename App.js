f170830897d65f3053c10868bc039467d7ba5d3d
KaareI <karlkaareltaniel.seppel@gmail.com>
2023-10-26 14:40:43 +0300

    Create profile screen

--- App.js
+++ App.js
@@ -1,103 +1,111 @@
-import React, {useEffect} from "react";
+import React, {useEffect} from 'react';
+
 import { Image } from "react-native";
 
-import Splash from "./src/screens/auth/Splash/";
-import Signup from "./src/screens/auth/Signup";
-import Signin from "./src/screens/auth/Signin";
+import { colors } from './src/utils/colors';
+import Splash from './src/screens/auth/Splash';
+import Signup from './src/screens/auth/Signup';
+import Signin from './src/screens/auth/Signin';
 
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-import Config from "react-native-config";
-import { colors } from "./src/utils/colors";
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
+import Settings from './src/screens/app/Settings';
+import CreateListing from './src/screens/app/CreateListing';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import Home from "./src/screens/app/Home"
-import Favorites from "./src/screens/app/Favorites"
-import Profile from "./src/screens/app/Profile"
+import Config from 'react-native-config';
 import ProductDetails from './src/screens/app/ProductDetails';
 
-const Tab = createBottomTabNavigator();
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={Profile} options={{headerShown: false}}></Stack.Screen>
+      <Stack.Screen name="Settings" component={Settings} options={{headerShown: false}}></Stack.Screen>
+      <Stack.Screen name="CreateListing" component={CreateListing} options={{headerShown: false}}></Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
 const Stack = createNativeStackNavigator();
 
-const Tabs = () => {
-  return(
-    <Tab.Navigator 
-    screenOptions={({route}) => ({
-      tabBarIcon: ({focused, color, size}) => {
-        let icon;
+const isSignedIn = true
 
-        if(route.name === "Home"){
-          icon = focused
-          ? require('./src/assets/tabs/home_active.png')
-          : require('./src/assets/tabs/home.png');
-        } else if (route.name === "Favorites"){
-          icon = focused
-          ? require('./src/assets/tabs/bookmark_active.png')
-          : require('./src/assets/tabs/bookmark.png');
-        } else if (route.name === "Profile"){
-          icon = focused
-          ? require('./src/assets/tabs/profile_active.png')
-          : require('./src/assets/tabs/profile.png');
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size }) => {
+        let icon;
+        if (route.name === 'Home') {
+          icon = focused 
+          ? require('./src/assets/tabs/home_active.png') : require('./src/assets/tabs/home.png');
+        } else if (route.name === 'Favorites') {
+          icon = focused 
+          ? require('./src/assets/tabs/bookmark_active.png') : require('./src/assets/tabs/bookmark.png');
+        } else if (route.name === 'Profile') {
+          icon = focused 
+          ? require('./src/assets/tabs/profile_active.png') : require('./src/assets/tabs/profile.png');
         }
+
         return <Image style={{width: 24, height: 24}} source={icon} />;
       },
       headerShown: false,
       tabBarShowLabel: false,
-      tabBarStyle: {borderTopColor: colors.lightGrey}
-    })}>
-      <Tab.Screen name="Home" component={Home}/>
+      tabBarStyle: {borderTopColor: colors.lightGray}
+  })}
+  >
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Favorites" component={Favorites}/>
-      <Tab.Screen name="Profile" component={Profile}/>
+      <Tab.Screen name="Profile" component={ProfileStack}/>
     </Tab.Navigator>
-  )
+  );
 }
 
+
 const App = () => {
-
-const isSignedIn = true;
-
-  useEffect (()=> {
+  useEffect(() => {
     GoogleSignin.configure({
-      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-      webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], 
+      webClientId: Config.GOOGLE_WEB_CLIENT_ID, 
       offlineAccess: true,
-      iosClientId: Config.GOOGLE_IOS_CLIENT_ID,
+      iosClientId: Config.GOOGLE_IOS_CLIENT_ID
     })
   }, [])
 
   const theme = {
-    colors: {
-      background: colors.white
-    },
-  };
+    colors: {background: colors.white},
+
+  }
 
   return (
     <SafeAreaProvider>
-<NavigationContainer theme={theme}>
-  <Stack.Navigator>
-{
-  isSignedIn ? (
-    <>
-    <Stack.Screen name="Tabs" component={Tabs} options={{headerShown: false}}/>
-    <Stack.Screen name="ProductDetails" component={ProductDetails} options={{headerShown: false}}/>
-    </>
-  ) : (
-    <>
-    <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-    <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}}/>
-    <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
-    </>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator>
+        {
+          isSignedIn ? (
+            <>
+            <Stack.Screen name="Tabs" component={Tabs} options={{headerShown: false}}/>
+            <Stack.Screen name="ProductDetails" component={ProductDetails} options={{headerShown: false}}/>
+            </>
+          ) : (
+            <>
+        <Stack.Screen name="SplashScreen" component={Splash} options={{headerShown: false}}/>
+        <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}}/>
+        <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
+            </>
+          )
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
+    </SafeAreaProvider>
   )
-  }
-  </Stack.Navigator>
-</NavigationContainer>
-</SafeAreaProvider>
-  );
 };
 
-export default React.memo(App);
+export default App
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -3,9 +3,9 @@ export const colors = {
     white: '#FFFFFF',
     orange: '#FCA34D',
     black: '#000000',
-    grey: '#8D9BB5',
-    lightGrey: '#DADADA',
-    darkGrey: '#3F4A59',
+    grey: "#8D9BB5",
+    lightGray: '#DADADA',
+    darkGrey: "#3F4A59",
     textGrey: "#606060",
     borderColor: "#707070"
 }
